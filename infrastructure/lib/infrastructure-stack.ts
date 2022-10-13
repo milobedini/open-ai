8f32de3e898867bae10a95bf7dbd900ec6e750bc
Milo Bedini <milobedini64@gmail.com>
2022-10-13 17:10:05 +0100

    infrastructure setup

---
+++ infrastructure/lib/infrastructure-stack.ts
@@ -0,0 +1,15 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+export class OpenaiInfrastructureStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const apiLambda = new lambda.Function(this, "ApiFunction", {
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset("../app/"),
+      handler: "",
+    });
+  }
+}
