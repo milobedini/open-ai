e86fb04790b87ef0928cdcf955574024fcd839a6
Milo Bedini <milobedini64@gmail.com>
2022-10-18 17:47:49 +0100

    styling phase 1

--- openai-client/components/Results.tsx
+++ openai-client/components/Results.tsx
@@ -7,32 +7,41 @@ interface ResultsProps {
 }
 
 const Results: React.FC<ResultsProps> = (props) => {
+  const resultSection = (label: string, body: any, extra: string) => {
+    if (extra != "list") {
+      return (
+        <div className={extra + " bg-slate-700 p-4 my-3 rounded-md"}>
+          <div className="text-slate-400 font-bold mb-4">{label}</div>
+          <div>{body}</div>
+        </div>
+      );
+    } else
+      return (
+        <div className="bg-teal-700  p-1 text- my-3 rounded-md px-2 text-sm">
+          <div>{body}</div>
+        </div>
+      );
+  };
   return (
     <>
-      <div>
-        <div>
-          <div>
-            <b>Prompt</b>
-          </div>
-          <div>{props.prompt}</div>
+      <div className="mb-6">
+        {resultSection("Prompt", <>{props.prompt}</>, "text-md")}
+        {resultSection("Tagline", <>{props.tagline}</>, "text-md")}
+        <div className="text-slate-400 font-bold mb-4">
+          <div className="text-slate-400 font-bold mb-4">Keywords</div>
         </div>
-        <div>
-          <div>
-            <b>Tagline</b>
-          </div>
-          <div>{props.tagline}</div>
-        </div>
-
-        <div>
-          <b>Keywords</b>
-        </div>
-        <div>
-          {props.keywords.map((keyword) => (
-            <div key={keyword}>#{keyword}</div>
-          ))}
+        <div className="flex flex-wrap gap-2 justify-center">
+          {props.keywords.map((keyword) =>
+            resultSection("Keywords", <>{`#${keyword}`}</>, "list")
+          )}
         </div>
       </div>
-      <button onClick={props.onBack}>Back</button>
+      <button
+        className="bg-gradient-to-r from-teal-400 to-blue-500 disabled:opacity-50 w-full p-2 rounded-md text-lg"
+        onClick={props.onBack}
+      >
+        Back
+      </button>
     </>
   );
 };
